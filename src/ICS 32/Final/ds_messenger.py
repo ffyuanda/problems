5ad83eba20758451bb5c94b794920079e51c4055
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-03-17 11:24:51 +0800

    ds_messenger.py basics almost finished, documentation finished

--- src/ICS 32/Final/ds_messenger.py
+++ src/ICS 32/Final/ds_messenger.py
@@ -9,22 +9,28 @@ HOST = "168.235.86.101"
 DataTuple = namedtuple('DataTuple', ['type', 'message', 'token'])
 
 
-class DirectMessengerError(Exception):
+class DMProtocolError(Exception):
+    """
+    Custom error class for DMProtocolError
+    """
     pass
 
 
-class DirectMessageError(Exception):
+class DirectMessengerError(Exception):
+    """
+    Custom error class for DirectMessenger
+    """
     pass
 
 
 class DirectMessage:
     """
-
+    The DirectMessage object using in sending messages.
     """
     def __init__(self):
         self.recipient = None
         self.message = None
-        self.timestamp = time.time()
+        self.timestamp = None
 
     def get_recipient(self):
         """
@@ -70,13 +76,18 @@ class DirectMessage:
 
 
 class DirectMessenger:
-    def __init__(self, dsuserver=None, username=None, password=None):
+    """
+    The class responsible for sending and receiving messages to and from another
+    user.
+    """
+    def __init__(self, dsuserver: str = None, username: str = None, password: str = None):
         self.token = None
         self.dsuserver = dsuserver
         self.username = username
         self.password = password
+        # DMProtocol for usage
         self.protocol = DMProtocol()
-
+        # Initialize the sock along with the DirectMessenger object
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.sock.connect((str(self.dsuserver), PORT))
@@ -85,31 +96,37 @@ class DirectMessenger:
             raise DirectMessengerError(msg) from e
 
     def send(self, message: str, recipient: str) -> bool:
-        # returns true if message successfully sent, false if send failed.
-
+        """
+        Send the message to the specified recipient.
+        Returns true if message successfully sent, false if send failed.
+        :param message: the input string that needs to be sent to another user
+        :param recipient: the user's name
+        :return: success, which indicates if the sending is successful
+        """
+        # join first to set self.token
         self.join(self.sock, self.username, self.password)
-        self.send_dm(self.sock, message, recipient)
+        success = self.send_dm(self.sock, message, recipient)
+        return success
 
     def join(self, sock: socket, username, password) -> None:
         """
         Joins the server and feed the retrieved token back to self.token
-        Uses send_join_processor() from DMProtocol
-        :param sock:
-        :param username:
-        :param password:
+        :param sock: currently working socket
+        :param username: username of current user
+        :param password: password of current user
         :return: None
         """
         join_msg = self.protocol.send_join_processor(username, password)
         self.protocol.sender(sock, join_msg)
         self.token = self.protocol.response(sock).token
 
-    def send_dm(self, sock: socket, message: str, recipient: str) -> None:
+    def send_dm(self, sock: socket, message: str, recipient: str) -> bool:
         """
         Wraps up message and recipient into a DirectMessage and send it
-        :param sock:
-        :param message:
-        :param recipient:
-        :return: None
+        :param sock: currently working socket
+        :param message: the input string that needs to be sent to another user
+        :param recipient: the user's name
+        :return: a bool indicates if the sending is successful
         """
 
         dm = DirectMessage()
@@ -117,42 +134,72 @@ class DirectMessenger:
         dm.set_recipient(recipient)
 
         message = self.protocol.send_directmessage_processor(self.token, dm)
-
         self.protocol.sender(sock, message)
-        self.protocol.response(sock)
+
+        response = self.protocol.response(sock)
+        if response.type == 'ok':
+            return True
+        else:
+            return False
 
     def retrieve_new(self) -> list:
-        # returns a list of DirectMessage objects containing all new messages
-        self.join(self.sock, self.username, self.password)
-        msg = self.protocol.request_unread_processor(self.token)
-
-        self.protocol.sender(self.sock, msg)
-        self.protocol.response(self.sock)
-
-    def retrieve_all(self) -> list:
-        # returns a list of DirectMessage objects containing all messages
+        """
+        Returns a list of DirectMessage objects containing all new messages
+        :return: a list of DirectMessage objects containing all new messages
+        """
 
         # make sure self.token is filled with the correct data
         self.join(self.sock, self.username, self.password)
-        msg = self.protocol.request_all_processor(self.token)
 
+        msg = self.protocol.request_unread_processor(self.token)
         self.protocol.sender(self.sock, msg)
-        self.protocol.response(self.sock)
+
+        dm_list = self.protocol.response(self.sock).message
+
+        return dm_list
+
+    def retrieve_all(self) -> list:
+        """
+        Returns a list of DirectMessage objects containing all messages
+        :return: a list of DirectMessage objects containing all messages
+        """
+
+        # make sure self.token is filled with the correct data
+        self.join(self.sock, self.username, self.password)
+
+        msg = self.protocol.request_all_processor(self.token)
+        self.protocol.sender(self.sock, msg)
+
+        dm_list = self.protocol.response(self.sock).message
+
+        return dm_list
 
 
 class DMProtocol:
+    """
+    A protocol class works collaboratively with DirectMessenger class.
+    It can also be modified to support other uses of server communication.
+    """
     def __init__(self):
         pass
 
-    def sender(self, sock: socket, msg: str):
+    def sender(self, sock: socket, msg: str) -> None:
+        """
+        A wrapper method to simplify the sending process.
+        :param sock: currently working socket
+        :param msg: the message needs to be sent
+        :return: None
+        """
         send = sock.makefile('w')
         send.write(msg + '\r\n')
         send.flush()
 
     def extract_json(self, json_msg: str) -> DataTuple:
-        '''
-        Call the json.loads function on a json string and convert it to a DataTuple object
-        '''
+        """
+        Call the json.loads function on a JSON string and convert it to a DataTuple object
+        :param json_msg: the input JSON str
+        :return: the DataTuple object contains json_msg's information
+        """
         try:
             type_list = ['error', 'ok']
 
@@ -163,14 +210,14 @@ class DMProtocol:
                 raise ValueError('Type is not ok or error.')
 
             type_ = json_obj['response']['type']
+
             # either 'message' or 'messages' will be returned from the server
             if 'message' in json_obj['response']:
-                # message here is a str
+                # message here is a str from server
                 message = json_obj['response']['message']
             elif 'messages' in json_obj['response']:
-                # message here is a list
+                # message here is a list of DirectMessage
                 message = json_obj['response']['messages']
-                # print(json_obj['response'])
 
             # when token is present for ok response
             if 'token' in json_obj['response']:
@@ -190,41 +237,28 @@ class DMProtocol:
 
     def response(self, sock: socket) -> DataTuple:
         """
-
-        :param sock: current connecting socket
+        It listens the raw response from the server at sock and convert them
+        to user friendly messages.
+        :param sock: current working socket
         :return: a DataTuple that contains the info from the server
         response JSON
         """
         recv = sock.makefile('r')
         resp = recv.readline()
-        # print(resp)
         resp = self.extract_json(resp)
 
         # DataTuple conversion
         if resp.type == 'ok':
+            # when sending DirectMessage
             if type(resp.message) is str:
 
-                if resp.message.startswith('Welcome back'):
-                    print("Successfully logged in.")
-                    print('Server says: {}\n'.format(resp.message))
-
-                elif resp.message.startswith('Welcome to'):
-                    print("Successfully registered in.")
-                    print('Server says: {}\n'.format(resp.message))
-
-                elif 'Direct message sent' in resp.message:
+                if 'Direct message sent' in resp.message:
                     print("Direct message successfully sent.")
                     print('Server says: {}\n'.format(resp.message))
 
-                elif 'Post' in resp.message:
-                    print("Post successfully sent.")
-                    print('Server says: {}\n'.format(resp.message))
-
+            # when retrieving DirectMessage
             elif type(resp.message) is list:
                 print('Messages successfully retrieved!')
-                print('Messages listed here:')
-                for m in resp.message:
-                    print(m)
 
         elif resp.type == 'error':
             print("An error occurs.")
@@ -239,13 +273,10 @@ class DMProtocol:
         Send format:
         {"join": {"username": "ohhimark", "password": "password123", "token": "user_token"}}
 
-        Example usage:
-        JSON_send = send_join_processor('0001', 'I am happy', '120000')
-
         :param username: the username of the user
         :param password: the password of the user
         :param token: the token that you want to verify with the server
-        :return: JSON string
+        :return: the formatted JSON string to server
         """
         output = '{{"join": {{"username": "{0}", "password": "{1}", "token": "{2}"}}}}'. \
             format(username, password, token)
@@ -253,8 +284,14 @@ class DMProtocol:
 
     def send_directmessage_processor(self, token, dm: DirectMessage) -> str:
         """
+        Process the inputs token and dm, and make them
+        into a JSON string.
+
         Send format:
         {"token":"user_token", "directmessage": {"entry": "Hello World!","recipient":"ohhimark", "timestamp": "1603167689.3928561"}}
+
+        :param token: the token of this message's sender
+        :param dm: the DirectMessage object that needs to be sent
         :return: the formatted JSON string to server
         """
         entry = dm.get_message()
@@ -269,20 +306,28 @@ class DMProtocol:
             format(token, entry, recipient, timestamp)
         return output
 
-    def request_unread_processor(self, token):
+    def request_unread_processor(self, token) -> str:
         """
+        Process the inputs token, and make it into a JSON string.
+
         Request format:
         {"token":"user_token", "directmessage": "new"}
+
+        :param token: the token of this message's sender
         :return: the formatted JSON string to server
         """
         output = '{{"token": "{0}", "directmessage": "new"}}'.format(token)
         return output
 
-    def request_all_processor(self, token):
+    def request_all_processor(self, token) -> str:
         """
+        Process the inputs token, and make it into a JSON string.
+
         Request format:
         {"token":"user_token", "directmessage": "all"}
-        :return:
+
+        :param token: the token of this message's sender
+        :return: the formatted JSON string to server
         """
         output = '{{"token": "{0}", "directmessage": "all"}}'.format(token)
         return output
@@ -290,15 +335,26 @@ class DMProtocol:
 
 if __name__ == '__main__':
 
+    # for test usage, run and see what happens!
+
     sender_dsuserver = HOST
     sender_username = 'The_Group_Sender'
-    sender_password = 'The_Group_Sender_password'
+    sender_password = 'The_Group_Sender_passworssd'
     sender = DirectMessenger(sender_dsuserver, sender_username, sender_password)
-    sender.send('something nice', 'The_Group_Receiver')
+    print(sender.send('something nice', 'The_Group_Receiver'))
+
+    print()
 
     receiver_username = 'The_Group_Receiver'
     receiver_password = 'The_Group_Receiver_password'
     receiver_dsuserver = HOST
     receiver = DirectMessenger(receiver_dsuserver, receiver_username, receiver_password)
-    receiver.retrieve_new()
-    receiver.retrieve_all()
+
+    print("receiver's new messages:")
+    print(receiver.retrieve_new())
+
+    print()
+
+    print("receiver's all messages:")
+    print(receiver.retrieve_all())
+
