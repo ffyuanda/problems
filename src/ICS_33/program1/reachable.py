9fd0bafc890bcb8d99c9630ccb29a5fa68be3cce
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-04-09 23:44:07 +0800

    program1 reachable finished

--- src/ICS_33/program1/reachable.py
+++ src/ICS_33/program1/reachable.py
@@ -4,23 +4,61 @@ from collections import defaultdict
 
 
 def read_graph(file : open) -> {str:{str}}:
-    pass
+    result_dict = defaultdict(set)
+    for line_number, text in enumerate(file):
+        result_dict[text[0]].add(text[2])
+    return dict(result_dict)
 
 
 def graph_as_str(graph : {str:{str}}) -> str:
-    pass
 
-        
+    output, converted_list = '', sorted([(source_node, sorted(list(des_nodes))) for
+                                         source_node, des_nodes in graph.items()], key=lambda x: x[0])
+    for source_node, des_nodes in converted_list:
+        output += "  {} -> {}\n".format(source_node, des_nodes)
+    return output
+
+
 def reachable(graph : {str:{str}}, start : str, trace : bool = False) -> {str}:
-    pass
+    reached_set = set()
+    exploring_list = list(start)
+    while True:
 
+        if len(exploring_list) == 0:
+            return reached_set
 
-
+        else:
+            if trace:
+                print("reached set    = " + str(reached_set))
+                print("exploring list = " + str(exploring_list))
+            curr_node = exploring_list.pop(0)
+            reached_set.add(curr_node)
+            if graph.get(curr_node, None) is not None:
+                for x in graph[curr_node]:
+                    if x not in reached_set:
+                        exploring_list.append(x)
+            if trace:
+                print("transferring node {} from the exploring list to the reached set".format(curr_node))
+                print("after adding all nodes reachable directly from {} but not already "
+                      "in reached, exploring = {}\n".format(curr_node, exploring_list))
 
 
 if __name__ == '__main__':
     # Write script here
-              
+    file = goody.safe_open('Input the file name detailing the graph: ', 'r', 'Illegal file name', default='graph1.txt')
+    graph = read_graph(file)
+    print("\nGraph: str (source node) -> [str] (sorted destination nodes)\n" + graph_as_str(graph))
+
+    while True:
+        node = input("Input one starting node (or input done): ")
+
+        if node == 'done':
+            break
+        elif node not in graph:
+            print("  Entry Error: '{}'; Illegal: not a source node\n  Please enter a legal String\n".format(node))
+        else:
+            trace = False if input("Input tracing algorithm option[True]: ") == "False" else True
+            print("From the starting node {}, its reachable nodes are: {}\n".format(node, reachable(graph, node, trace)))
     # For running batch self-tests
     print()
     import driver
--- src/tester.py
+++ src/tester.py
@@ -1,17 +1,4 @@
-# my_dict = {1:2, 3:4}
-# my_dict2 = {1:23, 3: 44}
-# my_list = my_dict2 + my_dict
-# print(my_list)
-# x = [1, 2, 3]
-# x[1] = x
-# print(x)
-from collections import defaultdict
-pairs = [('d', 15), ('d', 12), ('d', 10), ('d', 30), ('i', 15), ('i', 30), ('i', 15), ('r', 15), ('r', 8), ('r', 22), ('r', 30), ('r', 15), ('l', 20), ('l', 20), ('l', 15)]
-# some_dict = defaultdict(int)
-parties = ['d', 'l', 'r', 'i']
-some_list = [(party, list(map(lambda y: y[1], filter(lambda x: x[0] == party, pairs)))) for party in parties]
-some_list = [(i[0], sum(i[1])) for i in some_list]
-# some_list = list(map(lambda y: y[1], filter(lambda x: x[0] == 'd', pairs)))
-# some_list = [pairs[p][1] if pairs[p][0] == pairs[p - 1][0] else pairs[p][1] for p in range(0, len(pairs))]
-# some_dict = {pairs[p][0]: (pairs[p - 1][1] + pairs[p][1]) if pairs[p][0] == pairs[p - 1][0] else (pairs[p][1]) for p in range(1, len(pairs))}
-print(some_list)
+l = [1,2,3]
+print(l.pop(0))
+print(l
+      )
