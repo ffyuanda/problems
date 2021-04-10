af20b911c43fe03fc3f7f9fefe20630459f71028
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-04-10 17:08:23 +0800

    fa.py finished

--- src/ICS_33/program1/fa.py
+++ src/ICS_33/program1/fa.py
@@ -1,27 +1,59 @@
 import goody
+from collections import defaultdict
 
 
 def read_fa(file : open) -> {str:{str:str}}:
-    pass
+    output_dict = defaultdict(dict)
+    for row in file:
+        r_list = row.strip().split(';')
+        output_dict[r_list[0]] = {r_list[i]:r_list[i+1] for i in range(1, len(r_list), 2)}
+    return dict(output_dict)
 
 
 def fa_as_str(fa : {str:{str:str}}) -> str:
-    pass
+    output_str = ""
+    for state, trans_dict in sorted(fa.items(), key=lambda x: x[0]):
+        sorted_trans_list = sorted([(item[0], item[1]) for item in trans_dict.items()], key=lambda x: x[0])
+        output_str += "  {} transitions: {}\n".format(state, str(sorted_trans_list))
+    return output_str
 
-    
+
 def process(fa : {str:{str:str}}, state : str, inputs : [str]) -> [None]:
-    pass
+    output_list, curr = [state], state
+    for tran in inputs:
+        try:
+            curr = fa[curr][tran]
+        except KeyError:
+            output_list.append((tran, None))
+            break
+        output_list.append((tran, curr))
+    return output_list
 
 
 def interpret(fa_result : [None]) -> str:
-    pass
-
-
+    output_str = "Start state = {}\n".format(fa_result[0])
+    for i in range(1, len(fa_result)):
+        if fa_result[i][1] is None:
+            output_str += "  Input = {}; illegal input: simulation terminated\n".format(fa_result[i][0])
+        else:
+            output_str += "  Input = {}; new state = {}\n".format(fa_result[i][0], fa_result[i][1])
+    return output_str + "Stop state = {}\n".format(fa_result[-1][1])
 
 
 if __name__ == '__main__':
     # Write script here
-              
+    file = goody.safe_open('Input the file name detailing the Finite Automaton:', 'r', 'Illegal file name', default='faparity.txt')
+    fa = read_fa(file)
+    print("\nThe details of the Finite Automaton")
+    print(fa_as_str(fa))
+
+    input_file = goody.safe_open('Input the file name detailing groups of start-states and their inputs:', 'r', 'Illegal file name', default='fainputparity.txt')
+    for row in input_file:
+        r_list = row.strip().split(';')
+        fa_result = process(fa, r_list[0], r_list[1:])
+        print("\nFA: the trace from its start-state")
+        print(interpret(fa_result))
+
     # For running batch self-tests
     print()
     import driver
