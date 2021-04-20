44d13f170ec16a3676234cd3416ca76b62dc6102
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-04-21 00:24:26 +0800

    quiz3 all testcases passed

--- src/ICS_33/quiz3/trackhistory.py
+++ src/ICS_33/quiz3/trackhistory.py
@@ -1,24 +1,50 @@
 from collections import defaultdict
+import re
 
 
 class TrackHistory:
     def __init__(self):
-        pass
+        super().__setattr__("his_dict", defaultdict(list))
+        super().__setattr__("pattern", "^([a-z]+)(_prev)([0-9]*?)$")
 
-    
-    def __getattr__(self,name):
-        pass
+    def __setattr__(self, key, value):
+        if '_prev' in key:
+            raise NameError("key of attribute cannot contain \"_prev\"")
+        else:
+            self.his_dict[key].append(value)
+            super().__setattr__(key, value)
 
+    def __getattr__(self, name):
+        m = re.match(self.pattern, name)
+        if m is not None:
+            real_name = m.group(1)
+            if real_name not in self.his_dict.keys():
+                raise NameError(real_name + " does not exist as a key")
 
-    def __getitem__(self,index):
-        pass
+            if m.group(3) != '':  # has an index after _prev
+                ver_int = -int(m.group(3)) - 1
+                try:
+                    return self.his_dict[real_name][ver_int]
+                except IndexError:  # the index ver_int does not exist
+                    return None
+            else:  # has no index after _prev
+                try:
+                    return self.his_dict[real_name][-2]
+                except IndexError:  # the index ver_int does not exist
+                    return None
+        else:
+            raise NameError("The format of " + name + " is not right")
 
-    
-#    def __setattr__(self,name,value):
-#        pass
-
-
-
+    def __getitem__(self, index):
+        return_dict = defaultdict(int)
+        if index > 0:
+            raise IndexError
+        for value, versions in self.his_dict.items():
+            try:
+                return_dict[value] = versions[index-1]
+            except IndexError:
+                return_dict[value] = None
+        return dict(return_dict)
 
 
 if __name__ == '__main__':
@@ -27,7 +53,12 @@ if __name__ == '__main__':
 
     print('Start simple testing')
     print()
-    
+    # x = TrackHistory()
+    # x.six = 6
+    # x.six = 5
+    # x.six = 4
+    # x.six = 3
+
     import driver
     driver.default_file_name = 'bscq32S21.txt'
 #     driver.default_show_traceback=True
--- src/tester.py
+++ src/tester.py
@@ -1,30 +1,5 @@
-# matrix = [[1,2,8,9],
-#           [2,4,9,12],
-#           [4,7,10,13],
-#           [6,8,11,15]]
-#
-#
-# def find_num_in_matrix(matrix, find):
-#     bug_guy = []
-#     for x in matrix:
-#         bug_guy += x
-#     return find in bug_guy
+import re
 
-def add_space(in_str):
-    out_str = list(in_str + in_str.count(' ') * 2 * ' ')
-    point_1 = len(in_str) - 1
-    point_2 = len(out_str) - 1
-    while point_1 != point_2:
-        if out_str[point_1] != ' ':
-            out_str[point_2] = out_str[point_1]
-            point_1 -= 1
-            point_2 -= 1
-        else:
-            point_1 -= 1
-            point_2 -= 3
-            out_str[point_2+1:point_2+4] = '%20'
-    return str(out_str)
-
-if __name__ == '__main__':
-    add_space('w e e')
-    print(add_space('  '))
+pattern = "^([a-z]+)(_prev)([0-9]*?)$"
+m = re.match(pattern, "sasda_prev")
+print(len(m.group(3)))
