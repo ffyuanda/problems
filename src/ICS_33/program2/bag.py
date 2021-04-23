e6a6e3d2a489483ee4f07ecf5e936b6fe7025d57
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-04-23 11:37:50 +0800

    bag.py batch test passed

--- src/ICS_33/program2/bag.py
+++ src/ICS_33/program2/bag.py
@@ -6,14 +6,22 @@ import prompt
 
 class Bag:
 
-    def __init__(self, iterable: list=[]):
+    def __init__(self, iterable: list = list()):
         self.values = defaultdict(int)
         for i in iterable:
             self.values[i] += 1
 
     def __repr__(self) -> str:
         return_list = self.return_list()
-        return "Bag({})".format(str(return_list))
+        return "Bag({})".format(str(return_list)) if return_list != [] else "Bag()"
+
+    def __str__(self):
+        result_list = ["{}[{}]".format(key, count) for key, count in self.values.items()]
+        result_str = ''
+        for i in result_list:
+            result_str += i + ', '
+        result_str = result_str.rstrip(', ')
+        return "Bag({})".format(result_str)
 
     def __iter__(self):
         copy_dict = copy.deepcopy(dict(self.values))
@@ -21,8 +29,33 @@ class Bag:
         return iter(return_list)
 
     def __add__(self, other):
+        if other.__class__.__name__ != self.__class__.__name__:
+            raise TypeError('Not supported type concatenation')
         big_list = self.return_list() + other.return_list()
         return Bag(big_list)
+
+    def __len__(self):
+        return len(self.return_list())
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
+
+    def remove(self, key):
+        if key not in self.values.keys():
+            raise ValueError("The value needs to be removed does not exist")
+        else:
+            self.values[key] -= 1
+            if self.values[key] == 0:
+                self.values.pop(key)
+
+    def count(self, key):
+        return self.values[key]
+
+    def add(self, key):
+        self.values[key] += 1
+
+    def unique(self):
+        return len(self.values.keys())
 
     def return_list(self, iter_dict=None):
         if iter_dict is None:
@@ -30,9 +63,7 @@ class Bag:
         return_list = []
         for key, times in iter_dict.items():
             return_list += ([key] * times)
-        return return_list
-
-
+        return sorted(return_list)
 
 
 if __name__ == '__main__':
@@ -43,7 +74,11 @@ if __name__ == '__main__':
 
     b = Bag(['d','a','d','b','c','b','d'])
     # print(b.values)
-
+    # k = (repr(b).count('\''+v+'\'')==c for v,c in dict(a=1,b=2,c=1,d=3).items())
+    # s = (v + '[' + str(c) + ']' in str(b) for v, c in dict(a=1, b=2, c=1, d=3).items())
+    # print(k, s)
+    print(len(b))
+    print(b)
     print(repr(b))
     print(all((repr(b).count('\''+v+'\'')==c for v,c in dict(a=1,b=2,c=1,d=3).items())))
     for i in b:
--- src/tester.py
+++ src/tester.py
@@ -5,3 +5,10 @@
 # print(len(m.group(3)))
 
 print(['2'] * 3)
+
+from collections import defaultdict
+
+i = defaultdict(int)
+i['x'] = 1
+# i.pop('x')
+print(i)
