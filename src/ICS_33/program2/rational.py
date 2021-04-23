fe1520b9850919f0b28c399b1e660266721f1e1e
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-04-24 00:04:08 +0800

    rational.py passed batch test
    fuck this shit

--- src/ICS_33/program2/rational.py
+++ src/ICS_33/program2/rational.py
@@ -1,7 +1,22 @@
 from goody import irange, type_as_str
 import math
 
+
 class Rational:
+
+    def __init__(self, num=0, denom=1):
+
+        if type(num) is not int:
+            raise AssertionError("{}.{} num is not int: {}".format(self.__class__.__name__, self.__init__.__name__, num))
+        if type(denom) is not int:
+            raise AssertionError(
+                "{}.{} denom is not int: {}".format(self.__class__.__name__, self.__init__.__name__, denom))
+        if denom == 0:
+            raise AssertionError("{}.{} denominator should not be 0".format(self.__class__.__name__, self.__init__.__name__))
+
+        self.denom = abs(denom // Rational._gcd(abs(num), abs(denom)))
+        self.num = num // Rational._gcd(abs(num), abs(denom)) if denom > 0 else -(num // Rational._gcd(abs(num), abs(denom)))
+
     @staticmethod
     # Called as Rational._gcd(...); no self parameter
     # Helper method computes the Greatest Common Divisor of x and y
@@ -11,7 +26,7 @@ class Rational:
         while y != 0:
             x, y = y, x % y
         return x
-    
+
     @staticmethod
     # Called as Rational._validate_arithmetic(..); no self parameter
     # Helper method raises exception with appropriate message if type(v) is not
@@ -23,7 +38,7 @@ class Rational:
     def _validate_arithmetic(v : object, t : {type}, op : str, left_type : str, right_type : str):
         if type(v) not in t:
             raise TypeError('unsupported operand type(s) for '+op+
-                            ': \''+left_type+'\' and \''+right_type+'\'')        
+                            ': \''+left_type+'\' and \''+right_type+'\'')
 
     @staticmethod
     # Called as Rational._validate_relational(..); no self parameter
@@ -33,13 +48,173 @@ class Rational:
     def _validate_relational(v : object, t : {type}, op : str, right_type : str):
         if type(v) not in t:
             raise TypeError('unorderable types: '+
-                            'Rational() '+op+' '+right_type+'()') 
-                   
+                            'Rational() '+op+' '+right_type+'()')
 
    # Put all other methods here
 
+    def __repr__(self):
+        return "Rational({},{})".format(self.num, self.denom)
+
+    def __str__(self):
+        return '{}/{}'.format(self.num, self.denom)
+
+    def __neg__(self):
+        return Rational(-self.num, self.denom)
+
+    def __pos__(self):
+        return Rational(self.num, self.denom)
+
+    def __abs__(self):
+        return Rational(abs(self.num), abs(self.denom))
+
+    def __add__(self, other: "Rational" or int):
+        Rational._validate_arithmetic(other, {Rational, int}, '+', 'Rational', type_as_str(other))
+        if type(other) == int:
+            other = Rational(other, 1)
+        if self.denom == other.denom:
+            return Rational(self.num + other.num, self.denom)
+        else:
+            return Rational(self.num * other.denom + self.denom * other.num, self.denom * other.denom)
+
+    def __radd__(self, other: "Rational" or int):
+        return self.__add__(other)
+
+    def __sub__(self, other: "Rational" or int):
+        Rational._validate_arithmetic(other, {Rational, int}, '-', 'Rational', type_as_str(other))
+        return self.__add__(-other)
+
+    def __rsub__(self, other: "Rational" or int):
+        return -self.__sub__(other)
+
+    def __mul__(self, other: "Rational" or int):
+        Rational._validate_arithmetic(other, {Rational, int}, '*', 'Rational', type_as_str(other))
+        if type(other) == int:
+            result = Rational(self.num * other, self.denom)
+        else:
+            result = Rational(self.num * other.num, self.denom * other.denom)
+        return result
+
+    def __rmul__(self, other: "Rational" or int):
+        return self.__mul__(other)
+
+    def __truediv__(self, other: "Rational" or int):
+        Rational._validate_arithmetic(other, {Rational, int}, '/', 'Rational', type_as_str(other))
+        if type(other) == int:
+            return self.__mul__(Rational(1, other))
+        else:
+            return self.__mul__(Rational(other.denom, other.num))
+
+    def __rtruediv__(self, other: "Rational" or int):
+        if type(other) == int:
+            filp_result = self.__truediv__(other)
+            result = Rational(filp_result.denom, filp_result.num)
+            return result
+        return self.__truediv__(other)
+
+    def __pow__(self, power, modulo=None):
+        if type(power) != int:
+            raise TypeError('power is not int')
+        num = int(self.num ** abs(power))
+        denom = int(self.denom ** abs(power))
+        if power >= 0:
+            return Rational(num, denom)
+        else:
+            return Rational(denom, num)
+
+    def __bool__(self):
+        return False if self.num == 0 else True
+
+    def __eq__(self, other: "Rational" or int):
+        Rational._validate_relational(other, {Rational, int}, '==', type_as_str(other))
+
+        if type(other) == int:
+            r = Rational(other, 1)
+        else:
+            r = Rational(other.num, other.denom)
+        return self.denom == r.denom and self.num == r.num
+
+    def __lt__(self, other: "Rational" or int):
+        Rational._validate_relational(other, {Rational, int}, '<', type_as_str(other))
+        if type(other) == int:
+            return True if self.num / self.denom < other else False
+        else:
+            return True if self.num / self.denom < other.num / other.denom else False
+
+    def __gt__(self, other):
+        Rational._validate_relational(other, {Rational, int}, '>', type_as_str(other))
+        return not (self.__lt__(other) or self.__eq__(other))
+
+    def __le__(self, other):
+        Rational._validate_relational(other, {Rational, int}, '<=', type_as_str(other))
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __ge__(self, other):
+        Rational._validate_relational(other, {Rational, int}, '>=', type_as_str(other))
+        return self.__gt__(other) or self.__eq__(other)
+
+    def __getitem__(self, item: int or str):
+
+        if type(item) == str and item != "":
+
+            item = item.lower()
+            try:
+                if 'numerator'.index(str(item)) == 0:
+                    return self.num
+            except ValueError:
+                pass
+
+            try:
+                if 'denominator'.index(str(item)) == 0:
+                    return self.denom
+            except ValueError:
+                pass
+
+        elif type(item) == int:
+            if item == 0:
+                return self.num
+            elif item == 1:
+                return self.denom
+
+        raise TypeError('index is illegal')
+
+    def __call__(self, *args):
+        digits = args[0]
+        f_Rational = real_divide2(self.num, self.denom, digits)
+        return f_Rational
+
+    def __setattr__(self, key, value):
+        # print(self.__dict__)
+        if 'num' not in self.__dict__.keys() or 'denom' not in self.__dict__.keys():
+            super().__setattr__(key, value)
+        else:
+            raise NameError("no attributes can be modified/added")
 
 
+def real_divide2(dividend, divisor, digits=100):
+    trunc_digit = digits + 1
+    quo_str = '0'
+    add_decimal = True
+    while digits > 0:
+        if dividend // divisor == 0 and dividend % divisor != 0:
+            dividend *= 10
+            if add_decimal:
+                quo_str += '.'
+                add_decimal = False
+            quo_str += '0'
+            continue
+
+        quo, dividend = dividend // divisor, dividend % divisor
+        quo_str = quo_str[:-1] + str(quo)
+        if dividend == 0:
+            break
+
+        if add_decimal:
+            quo_str += '.'
+            add_decimal = False
+        else:
+            digits -= 1
+    deci_index = quo_str.index('.')
+    return quo_str[:deci_index] + quo_str[deci_index:deci_index + trunc_digit]
 
 
 # e ~ 1/0! + 1/1! + 1/2! + 1/3! ... 1/n!
@@ -58,25 +233,19 @@ def compute_pi(n):
         for i in r:
             answer *= i
         return answer
-    
+
     answer = Rational(1,2)
     x      = Rational(1,2)
     for i in irange(1,n):
         big = 2*i+1
-        answer += Rational(prod(range(1,big,2)),prod(range(2,big,2)))*x**big/big       
+        answer += Rational(prod(range(1,big,2)),prod(range(2,big,2)))*x**big/big
     return 6*answer
 
 
 if __name__ == '__main__':
     #Simple tests before running driver
-
-    x = Rational(8,29) 
-    print(x+x)
-    print(2*x)
-    print(x(30))
-    
     print()
-    import driver    
+    import driver
     driver.default_file_name = 'bscp22S21.txt'
 #     driver.default_show_exception=True
 #     driver.default_show_exception_message=True
--- src/tester.py
+++ src/tester.py
@@ -4,11 +4,124 @@
 # m = re.match(pattern, "sasda_prev")
 # print(len(m.group(3)))
 
-print(['2'] * 3)
+# print(['2'] * 3)
+#
+# from collections import defaultdict
+#
+# i = defaultdict(int)
+# i['x'] = 1
+# # i.pop('x')
+# print(i)
+# x = '12345'
+# x=x.lower()
+# print(x.index('13'))
+import math
+def truncate(number, digits) -> float:
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
 
-from collections import defaultdict
+x = 3.14159265358979323846264338327950228625461732116543
+# y = 3.14159265358979323846264338327950228625461732116543
+# print(str(int(x*100000000000000000000000000)))
+# print("{:.50f}".format(x))
+# print(format(x, '.60g'))
+# print(list(x))
+import decimal
 
-i = defaultdict(int)
-i['x'] = 1
-# i.pop('x')
-print(i)
+# print(math.ulp(x))
+
+
+def real_divide(dividend, divisor, digits=100):
+    quo_str = ''
+    count = 1
+    add_zero = 1
+    while digits > 0:
+        if dividend // divisor == 0 and dividend % divisor != 0:
+            dividend *= 10
+            if add_zero == 1:  # add_zero should only perform once
+                quo_str += '0'
+                if count == 0:  # after the decimal point
+                    digits -= 1
+            if count == 1:
+                quo_str += '.'
+                count = 0
+            continue
+
+        add_zero = 0
+
+        quo = dividend // divisor
+        quo_str += str(quo)
+
+        if count == 1:  # we are passing the decimal point
+            quo_str += '.'
+            count = 0
+
+        subtract = quo * divisor
+        if subtract == 0:
+            break
+
+        dividend = dividend - subtract
+        if count == 0: # after the decimal point
+            digits -= 1
+
+    return quo_str
+
+
+# def real_divide2(dividend, divisor, digits=100):
+#     quo_str = ''
+#     add_decimal = True
+#     add_zero = True
+#     while digits > 1:
+#         if dividend // divisor == 0 and dividend % divisor != 0:
+#             dividend *= 10
+#             if add_zero:
+#                 quo_str += '0'
+#                 if add_decimal:
+#                     quo_str += '.'
+#                     add_decimal = False
+#                 else:
+#                     digits -= 1
+#             continue
+#
+#         add_zero = False
+#
+#         quo, dividend = dividend // divisor, dividend % divisor
+#         quo_str += str(quo)
+#
+#         if add_decimal:
+#             quo_str += '.'
+#             add_decimal = False
+#         if not add_decimal:
+#             digits -= 1
+#     return quo_str
+
+def real_divide2(dividend, divisor, digits=100):
+    quo_str = '0'
+    add_decimal = True
+    while digits > 0:
+        if dividend // divisor == 0 and dividend % divisor != 0:
+            dividend *= 10
+            if add_decimal:
+                quo_str += '.'
+                add_decimal = False
+            quo_str += '0'
+            continue
+
+        quo, dividend = dividend // divisor, dividend % divisor
+        quo_str = quo_str[:-1] + str(quo)
+        if dividend == 0:
+            break
+
+        if add_decimal:
+            quo_str += '.'
+            add_decimal = False
+        else:
+            digits -= 1
+    return quo_str
+
+
+result = real_divide2(2, 3)
+print(result)
+print(len(result))
+print(len("14159265358979323846264338327950228625461732116543"))
+# print(math.pi)
