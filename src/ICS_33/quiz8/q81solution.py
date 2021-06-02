3169fa5150a0c86d415ed3d0ec78a4cdfac0db2a
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-06-02 16:59:56 +0800

    quiz8 almost finished
    Signed-off-by: ffyuanda <46557895+ffyuanda@users.noreply.github.com>

--- src/ICS_33/quiz8/q81solution.py
+++ src/ICS_33/quiz8/q81solution.py
@@ -1,5 +1,31 @@
 # Put your imports here
+import random, traceback
+from performance import Performance
+from nearestneighbor import closest_2d
+# Put your code for performance analysis here
+
+def doubling_signature(start=0, end=0) -> int or float:
+    while start <= end:
+        yield start
+        start *= 2
+
+def create_random(size: int = 0):
+    out_list = []
+    for i in range(size):
+        out_list.append((random.random(), random.random()))
+    return out_list
 
 
-
-# Put your code for performance analysis here 
+if __name__ == '__main__':
+    coor_list = []
+    x = doubling_signature(100, 25600)
+    for i in x:
+        coor_list = create_random(i)
+        title = f"Nearest Neighbor, size = {i}"
+        try:
+            p = Performance(lambda: closest_2d(coor_list), title=title)
+            p.evaluate()
+            p.analyze()
+            print()
+        except ValueError:
+            traceback.print_exc()
--- src/ICS_33/quiz8/q82solution.py
+++ src/ICS_33/quiz8/q82solution.py
@@ -1,5 +1,21 @@
 # Put your imports here
+import cProfile, pstats, random, traceback
+from nearestneighbor import closest_2d
 
+# Put your code for profile analysis here
 
+def create_random(size: int = 0) -> list:
+    out_list = []
+    for i in range(size):
+        out_list.append((random.random(), random.random()))
+    return out_list
 
-# Put your code for profile analysis here 
+coor_list = create_random(25600)
+
+def perform_task():
+    closest_2d(coor_list)
+
+cProfile.run("perform_task()", "profile")
+p = pstats.Stats('profile')
+p.strip_dirs().sort_stats('calls').print_stats(12)
+p.strip_dirs().sort_stats('time').print_stats(12)
