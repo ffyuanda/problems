6bcd2d9bcca897da2b2507b6d829d5bd9c78d586
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-06-03 15:19:24 +0800

    fix: modified q82solution.py for correct time calculation
    Signed-off-by: ffyuanda <46557895+ffyuanda@users.noreply.github.com>

--- src/ICS_33/quiz8/q82solution.py
+++ src/ICS_33/quiz8/q82solution.py
@@ -1,6 +1,7 @@
 # Put your imports here
-import cProfile, pstats, random, traceback
+import cProfile, pstats, random, traceback, gc
 from nearestneighbor import closest_2d
+from stopwatch import Stopwatch
 
 # Put your code for profile analysis here
 
@@ -19,3 +20,14 @@ cProfile.run("perform_task()", "profile")
 p = pstats.Stats('profile')
 p.strip_dirs().sort_stats('calls').print_stats(12)
 p.strip_dirs().sort_stats('time').print_stats(12)
+
+print("run without profiling started...")
+timer = Stopwatch()
+gc.disable()
+
+timer.start()
+closest_2d(coor_list)
+timer.stop()
+
+gc.enable()
+print('Time =', timer.read())
